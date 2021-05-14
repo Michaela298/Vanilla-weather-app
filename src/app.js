516206e7f55d7d20103dbbad31f78b08f8c86ff1
Michaela Šimková <michaelasimkova@Michaela-MacBook-Air-2.local>
2021-05-14 11:15:57 +0200

    Removed console.log

--- src/app.js
+++ src/app.js
@@ -70,7 +70,6 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "36794cb64ceccf9c27e4232061a9201a";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
 
